916711fcbe75ead3cdebb1719f3290df8853f496
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-03-11 23:29:14 +0200

    Initial Commit

---
+++ Odyssey/OdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdysseyApp.swift
+//  Odyssey
+//
+//  Created by Eugene Demenko on 11.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct OdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
